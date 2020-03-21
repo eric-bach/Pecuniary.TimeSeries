834e8281ffc9498401838dd9a97bcf83a6eda95e
Eric Bach <bach.eric@gmail.com>
2020-03-20 20:29:41 -0600

    Update role

--- src/Pecuniary.TimeSeries/Function.cs
+++ src/Pecuniary.TimeSeries/Function.cs
@@ -1,99 +1,84 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
 using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
-using Newtonsoft.Json;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 using EricBach.LambdaLogger;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace Pecuniary.TimeSeries
 {
     public class Function
     {
-        private static readonly HttpClient client = new HttpClient();
         private readonly AmazonDynamoDBClient _dynamoDbClient;
+        private readonly DynamoDBContext _dynamoDbContext;
+        // TODO Environment variable
         private static string tableName = "TimeSeries-5xjfz6mpa5g2rgwc47wfyqzjja-dev";
 
-        //public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
-        public async Task<ICollection<Dictionary<string, AttributeValue>>> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        public Function()
         {
-            //var location = await GetCallingIP();
-            //var body = new Dictionary<string, string>
-            //{
-            //    { "message", "hello world" },
-            //    { "location", location }
-            //};
-
-            //return new APIGatewayProxyResponse
-            //{
-            //    Body = JsonConvert.SerializeObject(body),
-            //    StatusCode = 200,
-            //    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            //};
-
-            return await GetAsync();
+            _dynamoDbClient = new AmazonDynamoDBClient(RegionEndpoint.USWest2);
+            _dynamoDbContext = new DynamoDBContext(_dynamoDbClient);
         }
 
-        private async Task<ICollection<Dictionary<string, AttributeValue>>> GetAsync()
+        public async Task<ICollection<TimeSeries>> FunctionHandler(ILambdaContext context)
+        {
+            return await GetAsync<TimeSeries>();
+        }
+
+        private async Task<ICollection<T>> GetAsync<T>()
         {
             Logger.Log($"Scanning DynamoDB {tableName} for all TimeSeries");
-            
-            var _dynamoDbClient = new AmazonDynamoDBClient(RegionEndpoint.USWest2);
-            List<Dictionary<string, AttributeValue>> items = new List<Dictionary<string, AttributeValue>>();
-            Logger.Log(_dynamoDbClient.ToString());
+        
+            ICollection<T> results = new List<T>();
             try
             {
-                var results =  await _dynamoDbClient.ScanAsync(new ScanRequest(tableName));
-                Logger.Log($"Found items: {results.Items.Count}");
-                items = results.Items;
+                var docs = await _dynamoDbClient.ScanAsync(new ScanRequest(tableName));
+                
+                Logger.Log($"Found items: {docs.Items.Count}");
+
+                foreach (var t in docs.Items.Select(i => DocumentToClass<T>(_dynamoDbContext, i)))
+                {
+                    results.Add(t);
+                }
             }
             catch (Exception e)
             {
                 Logger.Log(e.Message);
             }
 
-            //var context = new DynamoDBContext(_dynamoDbClient);
-            //var conditions = new List<ScanCondition>();
-            //var docs = await context.ScanAsync<TimeSeries>(conditions).GetRemainingAsync();
-
-            return items;
+            return results;
         }
 
-        private static async Task<string> GetCallingIP()
+        private static T DocumentToClass<T>(DynamoDBContext context, Dictionary<string, AttributeValue> obj)
         {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
-
-            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext: false);
-
-            return msg.Replace("\n", "");
+            var doc = Document.FromAttributeMap(obj);
+            return context.FromDocument<T>(doc);
         }
     }
 
+    // TODO Move to class
     public class TimeSeries
     {
-        public Guid Id { get; set; }
-        public decimal Close { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string Currency { get; set; }
-        public string Date { get; set; }
-        public decimal High { get; set; }
-        public decimal Low { get; set; }
-        public string Name { get; set; }
-        public decimal Open { get; set; }
-        public string Region { get; set; }
-        public string Symbol { get; set; }
-        public string Type { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public long Volume { get; set; }
+        public Guid id { get; set; }
+        public decimal close { get; set; }
+        public DateTime createdAt { get; set; }
+        public string currency { get; set; }
+        public string date { get; set; }
+        public decimal high { get; set; }
+        public decimal low { get; set; }
+        public string name { get; set; }
+        public decimal open { get; set; }
+        public string region { get; set; }
+        public string symbol { get; set; }
+        public string type { get; set; }
+        public DateTime updatedAt { get; set; }
+        public long volume { get; set; }
     }
 }
